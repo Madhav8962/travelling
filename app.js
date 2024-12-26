1ece717e0c373bafad6c0a2179204f6b5a961f74
Madhav8962 <madhav8962@gmail.com>
2024-12-26 19:27:07 +0530

    change

---
+++ app.js
@@ -0,0 +1,10 @@
+let navDialog =document.getElementById('nav-dialog');
+function handlemenu(){
+navDialog.classList.toggle('hidden');
+}
+
+function mini(){
+navDetail.classList.toggle('hidden');
+dropDown.classList.toggle('hidden');
+
+}
